56770c9766a9ca1d244e0287f0045633c41ef5c9
jhersbo <jhersbo@gmail.com>
2021-12-05 21:59:57 -0600

    on part 6 of activity (6.4.6)-2

--- views/error404.jsx
+++ views/error404.jsx
@@ -6,7 +6,12 @@ function Error404(){
         <Def>
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
-                <p>Sorry, these aren't the droids you're looking for...</p>
+                <div style={{textAlign: 'center'}}>
+                    <img src="/images/cat-404.jpg" alt="Cat 404"/>
+                    <div>
+                        Photo courtesy of <a href="https://www.pinterest.com/pin/857232110306378403/">novatrending.com</a>
+                    </div>
+                </div>
             </main>
         </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function Home(){
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="/images/flat-vegetables.jpg" alt="Vegetable Image" />
+                    <div>
+                        Photo by Ella Olsson from <a href="https://www.pexels.com/photo/flat-lay-photography-of-vegetable-salad-on-plate-1640777/">Pexels</a>
+                    </div>
+                </div>
                 <a href="/places">
                     <button className='btn-primary'>Places Page</button>
                 </a>
