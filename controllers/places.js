5d585d48af79585c3d0fe881def090669b7b98c3
jhersbo <jhersbo@gmail.com>
2021-12-07 14:50:48 -0600

    Jazzed up form page

--- controllers/places.js
+++ controllers/places.js
@@ -21,4 +21,32 @@ router.get('/new', (req, res)=>{
   res.render('places/new')
 })
 
+router.post('/', (req, res)=>{
+  res.send('POST')
+})
+
+router.get('/:id', (req, res)=>{
+  res.send('Place index #')
+})
+
+router.get('/:id/edit', (req, res)=>{
+  res.send('Edit form for a place')
+})
+
+router.put('/:id', (req, res)=>{
+  res.send('Make changes to existing place')
+})
+
+router.delete('/:id', (req, res)=>{
+  res.send('Delete a place')
+})
+
+router.post('/:id/rant', (req, res)=>{
+  res.send('Add rant to a place')
+})
+
+router.delete('/:id/rant/:rantId', (req, res)=>{
+  res.send('Delete a rant')
+})
+
 module.exports = router
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -6,6 +6,29 @@ function new_form(){
         <Def>
             <main>
                 <h1>Add a New Place</h1>
+                <form method='POST' action="/places">
+                    <div className='form-group'>
+                        <label htmlFor="name">Place Name</label>
+                        <input id='name' name='name' required className='form-control'/>
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor="pic">Place Picture</label>
+                        <input type="url" id='pic' name='pic' className='form-control'/>
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor="city">Enter City</label>
+                        <input type="text" id='city' name='city' className='form-control'/>
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor="state">Enter State</label>
+                        <input type="text" id='state' name='state' className='form-control'/>
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor="cuisines">Cuisines</label>
+                        <input type="text" name='cuisines' id='cuisines' required className='form-control'/>
+                    </div>
+                    <input type="submit" value='Add Place!' className='btn btn-primary'/>
+                </form>
             </main>
         </Def>
     )
